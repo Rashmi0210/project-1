473869c88707d729f6d684a42b3f0f4b54d48861
Rashmi0210 <114385788+Rashmi0210@users.noreply.github.com>
2024-04-24 16:17:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,5 @@
 <?php
 
-//lets make a connection with Addtocart database
-
-
 // Initialize MySQLi
 $con = mysqli_init();
  
@@ -25,15 +22,12 @@ echo "Connected successfully";
  
 // Now you can proceed with your database queries
 
- 
-
-
 if(isset($_GET["id"])){
     $product_id = $_GET["id"];
     $sql = "SELECT * FROM cart WHERE product_id = $product_id";
-    $result = $conn->query($sql);
+    $result = $con->query($sql);
     $total_cart = "SELECT * FROM cart";
-    $total_cart_result = $conn->query($total_cart);
+    $total_cart_result = $con->query($total_cart);
     $cart_num = mysqli_num_rows($total_cart_result);
 
     if(mysqli_num_rows($result) > 0){
@@ -42,7 +36,7 @@ if(isset($_GET["id"])){
         echo json_encode(["num_cart"=>$cart_num,"in_cart"=>$in_cart]);
     }else{
         $insert = "INSERT INTO cart(product_id) VALUES($product_id)";
-        if($conn->query($insert) === true){
+        if($con->query($insert) === true){
             $in_cart = "added into cart";
             echo json_encode(["num_cart"=>$cart_num,"in_cart"=>$in_cart]);
         }else{
@@ -55,7 +49,7 @@ if(isset($_GET["cart_id"])){
     $product_id = $_GET["cart_id"];
     $sql = "DELETE FROM cart WHERE product_id=".$product_id;
 
-    if($conn->query($sql) === TRUE){
+    if($con->query($sql) === TRUE){
         echo "Removed from cart";
     }
 }
